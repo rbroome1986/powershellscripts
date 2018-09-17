c6462579c555d897da848c6ec6ef522aa70ea9f7
rbroome1986 <43342726+rbroome1986@users.noreply.github.com>
2018-09-17 08:48:44 -0400

    Add files via upload

---
+++ ARCDeleteEmails.ps1
@@ -0,0 +1,14 @@
+#Script for determining number of items in users mailboxes from certain date range
+#Gets date for CSV formatting
+$CurrentDate = Get-Date
+$CurrentDate = $CurrentDate.ToString('yyyy-dd-hhmmss')
+#Require for local execution
+Set-ExecutionPolicy RemoteSigned
+#Must enter admin creds with proper Exchange permissions or this WILL NOT WORK! If unsure about RABC permisissions get with Ryan.
+$UserCredential = Get-Credential
+#Connection to O365
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+#Connection to O365
+Import-PSSession $Session -DisableNameChecking
+#Script below searchs all mailboxes and deletes for date range. 
+Get-Mailbox -identity brent.basbagill@arcind.com | Search-Mailbox -searchQuery 'Received:01/01/1990..12/31/2017 OR Sent:01/01/1990..12/31/2017' -deletecontent | Export-CSV -Path "$PSScriptRoot\DateQuery_$currentdate.csv"
---
+++ O365login.ps1
@@ -0,0 +1,9 @@
+#get started in O365
+
+Set-ExecutionPolicy RemoteSigned
+
+$UserCredential = Get-Credential
+
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+
+Import-PSSession $Session -DisableNameChecking
