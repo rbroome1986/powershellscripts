0f6697fe49e130d68f76dd5ae746c55f1b9957c0
Ryan H. Broome <rbroome@hmbnet.com>
2018-09-20 11:04:48 -0400

    Added user input for identity

--- ARCDeleteEmails.ps1
+++ ARCDeleteEmails.ps1
@@ -2,6 +2,8 @@
 #Gets date for CSV formatting
 $CurrentDate = Get-Date
 $CurrentDate = $CurrentDate.ToString('yyyy-dd-hhmmss')
+#email accounts we're deleting from
+$username = read-host "Enter username for email deletion"
 #Require for local execution
 Set-ExecutionPolicy RemoteSigned
 #Must enter admin creds with proper Exchange permissions or this WILL NOT WORK! If unsure about RABC permisissions get with Ryan.
@@ -11,4 +13,4 @@ $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri ht
 #Connection to O365
 Import-PSSession $Session -DisableNameChecking
 #Script below searchs all mailboxes and deletes for date range. 
-Get-Mailbox -identity brent.basbagill@arcind.com | Search-Mailbox -searchQuery 'Received:01/01/1990..12/31/2017 OR Sent:01/01/1990..12/31/2017' -deletecontent | Export-CSV -Path "$PSScriptRoot\DateQuery_$currentdate.csv"
+Get-Mailbox -identity $username | Search-Mailbox -searchQuery 'Received:01/01/1990..12/31/2017 OR Sent:01/01/1990..12/31/2017' -deletecontent | Export-CSV -Path "$PSScriptRoot\DateQuery_$currentdate.csv"
