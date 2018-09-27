5ffc921e0e7cdf75466f5a695ad626acd4728e09
Ryan H. Broome <rbroome@hmbnet.com>
2018-09-27 11:54:00 -0400

    perfect

--- alvisusercreation.ps1
+++ alvisusercreation.ps1
@@ -36,25 +36,23 @@
 
 <# Start Script #>
 #Import the CSV file for creating users
-$UserFile = Get-Item "\\ALV-FS01\NewUsers\newusr.csv"
+$UserFile = Get-FileName "\\ALV-FS1\New User Input\newusr.csv"
 $UserList = Import-csv $UserFile
 
 
 #Set password for users
 $securepassword = ConvertTo-SecureString "Password1234" -AsPlainText -Force
-import-module activedirectory
+
 
 #Create the user
-$userlist
-ForEach ($user in $userlist){
- $fname = $user.firstname
- $lname = $user.lastname
- $FInitial = $fname.substring(0,1)
- $ext = $user.ext
- $department = $user.department
+$userlist | ForEach {
+ $fname = $_.firstname
+ $lname = $_.lastname
+ $FInitial = $_.firstname.substring(0,1)
+ $ext = $_.ext
+ $department = $_.department
  $manager = Get-ADUser -Filter 'name -like "$._manager"' -Properties * | select samaccountname
- $title = $user.jobtitle
- 
+ $title = $_.jobtitle
  #$InternOrStaff = $Person.InternOrStaff
  #$Department = $Person.Department
  #$Ext = $Person.Extension
@@ -65,30 +63,43 @@ ForEach ($user in $userlist){
  $email = $fname + "." +$lname + "@alvis180.org"
  $email2 = $SAM + "@alvis180.org"
  $driveletter = "H:"
- $user = Get-ADUser -Filter LDAPFilter "(sAMAccountName=$SAM)"
 
  #Set OU based on if they're an intern or staff just as an example. You can put whatever in here
  #if ($InternOrStaff -eq "I") { $Path = 'OU=ARCUsers,DC=internal,DC=alvis180.org,DC=com'}
  #if ($InternOrStaff -eq "S") { $Path = '<Enter OU Here ex: (OU=Test,OU=Company,DC=hmbnet,DC=com)>'}
  $Path = 'OU=TEST,DC=internal,DC=alvis.local,DC=com'
- }
 
 
  #check to see if the user already exists
- if($user -ne $null){"User $SAM already exists."}
+ if(dsquery user -samid $SAM){write-host "User $SAM already exists."}
  else{
 
   #Actually Creating the user
   New-ADUser -Name $username -Displayname $username -GivenName $fname -Surname $lname -SamAccountName $SAM `
   -UserPrincipalName $username -AccountPassword $securepassword -EmailAddress $email  `
-  -manager $manager -OfficePhone $ext -department $department -ChangePasswordAtLogon $True `
-  -Path $Path -homedrive $driveletter -HomeDirectory "\\ALV-VMHOST2\HomeFolders\$SAM" -PassThru | Disable-ADAccount
+  -manager $manager -telephoneNumber $ext -department $department `
+  -Path $Path -homedrive $driveletter -HomeDirectory "\\internal.alvis180.org\HomeFolders\HomeFolders\$SAM" -PassThru | Disable-ADAccount
   Write-Host "User $username created"
 
   #Set user attributes
   set-aduser -identity $SAM -Add @{ProxyAddresses="SMTP:"+$email}
   set-aduser -identity $SAM -Add @{ProxyAddresses="smtp:"+$email2}
+
+  #If Ext is populated then set Extension
+  if ($Ext){
+   set-aduser -identity $SAM -Add @{telephoneNumber="$Ext"}
   }
+  #Change password on login
+  set-aduser -identity $SAM -ChangePasswordAtLogon $True
+
+  #Create user share folder and apply permissions
+  New-Item -Path "\\internal.alvis180.org\HomeFolders\HomeFolders\$SAM" -type directory -Force
+  $Acl = Get-Acl "\\internal.alvis180.org\HomeFolders\HomeFolders\$SAM"
+  $Ar = New-Object System.Security.AccessControl.FileSystemAccessRule($SAM,"Fullcontrol","Allow")
+  $Acl.SetAccessRule($Ar)
+  Set-Acl "\\internal.alvis180.org\HomeFolders\HomeFolders\\$SAM" $Acl
+ }
+}
 #beep boop#
 Write-Host "Beep boop user creation finished" -confirm
 #delete csv when finished#
